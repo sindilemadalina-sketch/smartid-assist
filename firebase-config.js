4c8955cac8a1f075e3d96ac44bd17566a9f57e72
smartid-training <sindilemadalina@gmail.com>
2026-07-15 15:48:56 +0300

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDyRr9PKvwsZ7QonqFuDFjRShVR8NqN16k",
-  authDomain: "smart-id-assist.firebaseapp.com",
-  projectId: "smart-id-assist",
-  storageBucket: "smart-id-assist.firebasestorage.app",
-  messagingSenderId: "812376610238",
-  appId: "1:812376610238:web:aaa03b4bd1c7bfd22c55ef"
-};
