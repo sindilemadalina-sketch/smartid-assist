1d5fa25ca53e8fad9e40aaf472cb298fb77b9ee9
smartid-training <sindilemadalina@gmail.com>
2026-07-15 15:50:47 +0300

    Add files via upload

---
+++ stores-seed.js
@@ -0,0 +1,779 @@
+export const INITIAL_STORES = [
+  {
+    "id": "3051",
+    "name": "Barlad",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "3052",
+    "name": "Ploiesti 2",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "3050",
+    "name": "Sfantu Gheorghe",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "27",
+    "name": "Colosseum",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "40",
+    "name": "Piatra Neamt",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "26",
+    "name": "Drobeta",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "3053",
+    "name": "Alba Iulia",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "3054",
+    "name": "Craiova",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "3055",
+    "name": "Arad",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "42",
+    "name": "Roman",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "163",
+    "name": "Targu Jiu",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "10",
+    "name": "Iasi Felicia",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "3056",
+    "name": "Pitesti",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "1",
+    "name": "Chiajna",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "33",
+    "name": "Galati",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "41",
+    "name": "Rm. Valcea",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "11",
+    "name": "Braila",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "12",
+    "name": "Suceava",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "3061",
+    "name": "City Park",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "3058",
+    "name": "Bratianu",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "3059",
+    "name": "Alexandriei",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "3066",
+    "name": "Pantelimon",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "3064",
+    "name": "Cluj 2",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "3060",
+    "name": "Bacau",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "3057",
+    "name": "Ploiesti Afi",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "3065",
+    "name": "Lujerului",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "3062",
+    "name": "Drobeta 2",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "18",
+    "name": "Oradea Lotus",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "46",
+    "name": "Zalau",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "19",
+    "name": "Buzau",
+    "category": "carrefour",
+    "format": "hiper",
+    "active": true
+  },
+  {
+    "id": "317",
+    "name": "Iasi Valea Lupului",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "280",
+    "name": "Gilau",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "338",
+    "name": "Timisoara Bucovinei",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "181",
+    "name": "Chilia Veche",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "826",
+    "name": "Timisoara Rebreanu",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "286",
+    "name": "Caramfil",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "352",
+    "name": "Iasi Niciman",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "332",
+    "name": "Husi",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "118",
+    "name": "Braila",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "65",
+    "name": "Zalau",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "224",
+    "name": "Berceni",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "867",
+    "name": "Iasi Alexandru",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "124",
+    "name": "Galati Dunarea",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "125",
+    "name": "Cultural (Obregia)",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "114",
+    "name": "Timisoara 3",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "411",
+    "name": "Brasov Cosmos",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "394",
+    "name": "Regie",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "115",
+    "name": "Dorobanti",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "393",
+    "name": "Craiova Fagaras",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "388",
+    "name": "Subcetate",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "840",
+    "name": "Brasov Zorilor",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "872",
+    "name": "Targu Mures",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "454",
+    "name": "Cloud9",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "856",
+    "name": "Cluj Ferdinand",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "457",
+    "name": "Buzias",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "455",
+    "name": "Cosmopolis Plaza",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "166",
+    "name": "Brasov Privilegio",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "461",
+    "name": "Ipotesti Suceava",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "852",
+    "name": "Ferdinand Bucuresti",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "858",
+    "name": "Cluj Zorilor",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "121",
+    "name": "Pitesti",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "851",
+    "name": "Minis Titan",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "844",
+    "name": "Rasnov",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "815",
+    "name": "Giurgiu",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "467",
+    "name": "Ciorogarla Darvari",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "481",
+    "name": "Victor Brauner",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "486",
+    "name": "Bragadiru Cristalului",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "487",
+    "name": "Volovat",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "496",
+    "name": "Onesti",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "198",
+    "name": "Lugoj2",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "871",
+    "name": "Orsova",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "57",
+    "name": "Lugoj 1",
+    "category": "carrefour",
+    "format": "super",
+    "active": true
+  },
+  {
+    "id": "316",
+    "name": "Cosmopolis 2",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "315",
+    "name": "Iasi Nicolina",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "319",
+    "name": "Sibiu",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "325",
+    "name": "Oradea Republicii",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "327",
+    "name": "Brasov Muresenilor",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "309",
+    "name": "Cluj Septimiu Albini",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "329",
+    "name": "Iasi Gemi",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "453",
+    "name": "Iasi Palas",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "328",
+    "name": "Mario Plaza",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "464",
+    "name": "Cosmopolis 3",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "302",
+    "name": "Craiova Valea Rosie",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "390",
+    "name": "Cotroceni One",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "476",
+    "name": "Constanta Stefan cel Mare",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "477",
+    "name": "Joy Residence",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "478",
+    "name": "Navodari Biruintei",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "876",
+    "name": "Otopeni Aeroport",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "479",
+    "name": "Bucuresti Basarabiei",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "480",
+    "name": "Voluntari 1D",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "483",
+    "name": "Bucuresti Piata Rosetti",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "484",
+    "name": "Bucuresti Penes Curcanul",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "485",
+    "name": "Brasov Galerie",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "489",
+    "name": "Buzau Unirii 48A",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "490",
+    "name": "Bacau",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "491",
+    "name": "Bucuresti WIN Herastrau",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "493",
+    "name": "Constanta Dezrobirii",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "488",
+    "name": "Brasov Republicii",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "494",
+    "name": "Calarasi",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "497",
+    "name": "Adjud",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "495",
+    "name": "Bolotesti",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "492",
+    "name": "Maicanesti",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "475",
+    "name": "Voluntari 2",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "498",
+    "name": "Targu Frumos",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "499",
+    "name": "Vicovu de Jos",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "500",
+    "name": "Cluj 21 Decembrie",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "502",
+    "name": "Drobeta",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "503",
+    "name": "Focsani",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "501",
+    "name": "Cluj Dionisie",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "506",
+    "name": "Dealu Tugulea",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  },
+  {
+    "id": "504",
+    "name": "Sacele",
+    "category": "carrefour",
+    "format": "express",
+    "active": true
+  }
+];
